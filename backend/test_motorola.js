3f2046b6276cc3011a720b64fb9c6b7c4ada77ae
Hemanth <23r21a04c7@mlrit.ac.in>
2026-04-09 23:07:52 +0530

    Final updated project

--- backend/test_motorola.js
+++
@@ -1,9 +0,0 @@
-const liveViewScraper = require('./scrapers/liveViewScraper');
-
-async function testResolution() {
-    console.log("Resolving Amazon URL for 'MOTOROLA g35 5G'...");
-    const resolution = await liveViewScraper.resolveProductURL('amazon', 'MOTOROLA g35 5G');
-    console.log(resolution);
-}
-
-testResolution().catch(console.error);
